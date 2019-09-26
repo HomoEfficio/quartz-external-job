d4c80e8348abb1eaf17ba53a6f3cc634876dea62
Homo Efficio <homo.efficio@gmail.com>
2019-09-26 23:43:11 +0900

    Added quartz-scheduler module

---
+++ quartz-scheduler/src/test/java/io/homo_efficio/quartz/scheduler/QuartzSchedulerApplicationTests.java
@@ -0,0 +1,16 @@
+package io.homo_efficio.quartz.scheduler;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QuartzSchedulerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
