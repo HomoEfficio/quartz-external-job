cca73fca322cb062ee54dc220325ca26bc4a635a
Homo Efficio <homo.efficio@gmail.com>
2019-09-29 18:58:11 +0900

    Autowire service into the external Job class

--- quartz-job/src/main/java/io/homo_efficio/quartz/job/RemoteSimpleJob.java
+++ quartz-job/src/main/java/io/homo_efficio/quartz/job/RemoteSimpleJob.java
@@ -1,25 +1,30 @@
 package io.homo_efficio.quartz.job;
 
 import io.homo_efficio.quartz.scheduler.HelloService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.Job;
+import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
-import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * @author homo.efficio@gmail.com
  * created on 2019-09-28
  */
 @Slf4j
+@RequiredArgsConstructor
 public class RemoteSimpleJob implements Job {
 
-    @Autowired
-    private HelloService helloService;
+    private final HelloService helloService;
+
 
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
-        log.info("OOO {} executed..", this.getClass().getSimpleName());
+        log.info("OOO REMOTE JOB [{}] executed.", this.getClass().getSimpleName());
+        JobDataMap mergedJobDataMap = context.getMergedJobDataMap();
+        mergedJobDataMap.forEach((k, v) -> log.info("OOOOO {}: {}", k, v));
+
         helloService.sayHello();
     }
 }
