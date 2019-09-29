227847cfce5fa0918cd65fc7b371326d4b6e70b4
Homo Efficio <homo.efficio@gmail.com>
2019-09-29 21:32:07 +0900

    Delete unnecassary save() method

--- quartz-scheduler/src/main/java/io/homo_efficio/quartz/scheduler/repository/MemberRepository.java
+++ quartz-scheduler/src/main/java/io/homo_efficio/quartz/scheduler/repository/MemberRepository.java
@@ -10,6 +10,4 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    @Override
-    <S extends Member> S save(S entity);
 }
